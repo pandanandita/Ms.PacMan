540ba1c60bbb557f7afa495f1ae4b9ca24b12e23
Nandita <pnandita42@gmail.com>
2026-03-10 23:27:27 -0500

    Project 4

--- build.bash
+++ build.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -u -e
-javac Game.java View.java Controller.java Model.java Tile.java Json.java MsPacman.java
-java Game
+javac -d bin Game.java View.java Controller.java Model.java Tile.java Json.java MsPacman.java Sprite.java
+java -cp bin Game
